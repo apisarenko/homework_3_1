81486a9a3ded576a117db831caa6c95751b4fa8f
apisarenko <47703396+apisarenko@users.noreply.github.com>
2019-04-05 19:44:30 +0300

    Add files via upload

---
+++ decorator.py
@@ -0,0 +1,25 @@
+import datetime
+import time
+
+
+def path_out(path):
+    def decorator(old_func):
+        def new_func(*args, **kwargs):
+            start = datetime.datetime.now()
+            out_in = old_func(*args, **kwargs)
+            out = out_in * 32
+            output_line = 'Время вызова: ' + str(start) + '\n' + \
+                          'Имя функции: ' + (f'{old_func.__name__}') + '\n' + \
+                          'Аргумент вызова: ' + str(args) + '\n' + \
+                          'Возвращаемое значение: ' + str(out)
+            with open(path, 'w', encoding='utf8') as file:
+                file.write(str(output_line))
+            return out
+        return new_func
+    return decorator
+
+
+@path_out('out.txt')
+def foo(x):
+    return x**2
+foo(4)
